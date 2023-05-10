14150bddbb6f3179f863bd070910d84c6ddf52ae
LuigiRibeiro <luigimacielribeiro@gmail.com>
2023-05-10 17:53:42 -0300

    update lib

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 # This is a test lib retention_plans.
+from retention_plans import check_retention
+import datetime as dt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def test_retention_plans():
+    list_tests = [
+        (dt.date(2023, 5, 5), 'standard', True),
+        (dt.date(2023, 5, 5), 'standard', False), # Force erro
+        (dt.date(2023, 5, 5), 'gold', True),
+        (dt.date(2023, 5, 5), 'platinum', True),
+        (dt.date(2022, 4, 30), 'standard', False),
+        (dt.date(2022, 4, 30), 'gold', False),
+        (dt.date(2023, 4, 30), 'gold', True),
+        (dt.date(2022, 4, 30), 'platinum', False),
+        (dt.date(2010, 12, 31), 'standard', False),
+        (dt.date(2010, 12, 31), 'gold', False),
+        (dt.date(2010, 12, 31), 'platinum', False),
+        (dt.date(2020, 12, 31), 'platinum', True),
+    ]
+
+    test_success = 0
+    test_erro = 0
+
+    for test in list_tests:
+        if check_retention(test[0], test[1]) == test[2]:
+            test_success += 1
+        else:
+            test_erro += 1
+
+    print('Results test:')
+    print(f'Success: {test_success}')
+    print(f'Erro: {test_erro}')
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test_retention_plans()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- retention_plans.py
+++ retention_plans.py
@@ -1,13 +1,57 @@
 import datetime as dt
 
-list_retention_plans = ['Standard', 'Gold', 'Platinum ']
+list_retention_plans = ['standard', 'gold', 'platinum ']
 
 
 def last_day_month(year, month):
-    date_control = dt.datetime(year, month, 1)
+    date_control = dt.date(year, month, 1)
     next_month = date_control.replace(day=28) + dt.timedelta(days=4)
     return next_month - dt.timedelta(days=next_month.day)
 
 
-def check_retention():
-    pass
+def check_retention(date, retention_plan):
+    """
+    Determines whether an ERP snapshot should be kept or removed based on a specified retention plan.
+
+    Args:
+    - retention_plan (str): the retention plan to consider, which can be 'standard', 'gold', or 'platinum'.
+    - date (datetime.date): the date of the snapshot to be checked.
+
+    Returns:
+    - bool: True if the snapshot should be retained, False if it should be deleted.
+
+    Raises:
+    - ValueError: if the provided plan is not recognized.
+    - ValueError: if the date is not valid.
+    """
+
+    if retention_plan not in ['standard', 'gold', 'platinum']:
+        raise 'Plan is not recognized'
+
+    try:
+        day_plan = date.day
+        month_plan = date.month
+        year_plan = date.year
+    except:
+        raise 'Date is not valid'
+
+    today = dt.date.today()
+    difference_dates = today - date
+    difference_days = difference_dates.days
+    difference_months = (today.year - date.year) * 12 + (today.month - date.month)
+    difference_years = today.year - date.year
+
+    if retention_plan == 'standard':
+        return True if difference_days <= 42 else False
+    elif retention_plan == 'gold':
+        if last_day_month(year_plan, month_plan) == date:
+            return True if difference_months <= 12 else False
+        else:
+            return True if difference_days <= 42 else False
+    else:
+        if dt.date(year_plan, 12, 31) == date:
+            return True if difference_years <= 7 else False
+        elif last_day_month(year_plan, month_plan) == date:
+            return True if difference_months <= 12 else False
+        else:
+            return True if difference_days <= 42 else False
